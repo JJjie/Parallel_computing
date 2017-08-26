6811f6deefdba4bf954d428ff64600db2a66fa53
JJjie <hwj950504@163.com>
2017-08-26 15:29:32 +0800

    debug makefile

---
+++ 5_flowParallel/build/CMakeFiles/flowParallel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flowParallel.dir/flowParallel_generated_test.cu.o"
+  "flowParallel.pdb"
+  "flowParallel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/flowParallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
