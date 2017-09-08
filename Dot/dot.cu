8f1624fbdb9cdf98092eb629caa480979d7067db
胡文杰 <hwj950504@163.com>
2017-09-08 11:26:08 +0800

    debug

--- Dot/dot.cu
+++ Dot/dot.cu
@@ -13,7 +13,7 @@ const int blocksPerGrid = imin(32, (N+threadsPerBlock-1)/threadsPerBlock);
 __global__ void dot(float *a, float *b, float *c){
     __shared__ float cache[threadsPerBlock];
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    int cacheIndex = threadIdx.x
+    int cacheIndex = threadIdx.x;
 
     float temp = 0;
     while (tid < N){
@@ -81,7 +81,7 @@ int main(){
 
     dot<<<blocksPerGrid, threadsPerBlock>>>(dev_a, dev_b, dev_partial_c);
 
-    cudastatus = cudaMemcpy(c, dev_partial_c, blocksPerGrid*sizeof(float), cudaMemcpyDeviceToHost);
+    cudastatus = cudaMemcpy(partial_c, dev_partial_c, blocksPerGrid*sizeof(float), cudaMemcpyDeviceToHost);
     if (cudastatus != cudaSuccess){
         printf("cuda memcpy fail!\n");
         goto Error;
